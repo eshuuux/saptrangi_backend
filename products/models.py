5fbe2374be32bd1e671a1bd9ec0ab4daee459855
eshuuux <ashishkhoteak47@gmail.com>
2025-11-19 00:18:11 +0530

    Initial Django project commit

---
+++ products/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here.
+
+class Carousel(models.Model):
+    name=models.CharField(max_length=100)
+    desktop_image=models.URLField(max_length=500)       # Desktop Image
+    mobile_image=models.URLField(max_length=500)       # Mobile Image
+    class Meta():
+        db_table='carousel'
+
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    mrp=models.IntegerField()     # Maximum Retail Price
+    price=models.IntegerField()
+    rating=models.FloatField()
+    discount=models.IntegerField()
+    product_images=ArrayField(models.URLField(max_length=500), blank=True, default=list)
+    category=models.CharField(max_length=100)
+    top_picks=models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta():
+        db_table='product'
+    
+class Banner(models.Model):
+    name=models.CharField(max_length=100)
+    banner_image=models.URLField(max_length=500)    # Banner Image
+    class Meta():
+        db_table='banner'
