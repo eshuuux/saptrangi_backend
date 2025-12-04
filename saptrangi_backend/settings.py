554bc931bb1cf8f26feb374aaaa735a199afd588
eshuuux <ashishkhoteak47@gmail.com>
2025-12-05 00:05:51 +0530

    CORS Updations

--- saptrangi_backend/settings.py
+++ saptrangi_backend/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'users',
     'products',
     'orders',
     'payments',
+    
 ]
 
 
@@ -57,6 +60,8 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,6 +71,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173/',
+    'https://saptrangi.netlify.app/'
+]
+
 
 ROOT_URLCONF = 'saptrangi_backend.urls'
 
