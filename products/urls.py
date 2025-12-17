3218c7ca8033784b44287756a2a1c0b4a0b35ba1
eshuuux <ashishkhoteak47@gmail.com>
2025-12-18 01:21:46 +0530

    API Routing Adjustment, 5

--- products/urls.py
+++ products/urls.py
@@ -5,11 +5,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", ProductView.as_view()),                      # GET, POST
-    path("<slug:slug>/", ProductDetailBySlug.as_view()),  # GET
-    path("category/<str:category>/", ProductDetailByCategory.as_view()),
     path("home/", HomeView.as_view()),
     path("banners/", BannerView.as_view()),
     path("carousel/", CarouselView.as_view()),
+    path("category/<str:category>/", ProductDetailByCategory.as_view()),
+    path("<slug:slug>/", ProductDetailBySlug.as_view()),  # GET
+    path("", ProductView.as_view()),                      # GET, POST
 ]
 
